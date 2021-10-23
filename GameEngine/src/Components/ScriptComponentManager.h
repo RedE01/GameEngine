756c91dad0cda2ecaffaf48eb10878121e630aea
RedE01 <e.sternvik@gmail.com>
2021-10-23 21:39:42 +0200

    Add ScriptComponentManager

---
+++ GameEngine/src/Components/ScriptComponentManager.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <vector>
+#include <functional>
+#include <entt/entity/registry.hpp>
+
+namespace GameEngine {
+
+	namespace ScriptComponentManagerInternal {
+		template <class T>
+		void updateComponentsOfType(entt::registry& registry) {
+			registry.view<T>().each([](T& component){
+				component.update();
+			});
+		}
+	}
+
+	class ScriptComponentManager {
+	public:
+		template<typename T>
+		void registerScriptComponent() {
+			m_scriptComponentUpdateFunctions.push_back(ScriptComponentManagerInternal::updateComponentsOfType<T>);
+		}
+
+	private:
+		void updateScriptComponents(entt::registry& registry) {
+			for(auto& updateFn : m_scriptComponentUpdateFunctions) {
+				updateFn(registry);
+			}
+		}
+
+	private:
+		std::vector<std::function<void(entt::registry&)>> m_scriptComponentUpdateFunctions;
+
+		friend class Scene;
+	};
+
+}
