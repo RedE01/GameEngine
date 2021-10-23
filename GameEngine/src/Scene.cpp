a2bb6057e83960fb11fb44660e28b539c5c2c275
RedE01 <e.sternvik@gmail.com>
2021-10-23 21:42:26 +0200

    Implement Scene class

--- GameEngine/src/Scene.cpp
+++ GameEngine/src/Scene.cpp
@@ -2,4 +2,12 @@
 
 namespace GameEngine {
 
+	Entity Scene::createEntity() {
+		entt::entity entity = m_entityRegistry.create();
+		return Entity(&m_entityRegistry, entity);
+	}
+
+	void Scene::destroyEntity(const Entity& entity) {
+		m_entityRegistry.destroy(entity.getID());
+	}
 }
--- GameEngine/src/Scene.h
+++ GameEngine/src/Scene.h
@@ -1,9 +1,18 @@
 #pragma once
+#include "Entity.h"
 
 namespace GameEngine {
-
+	
 	class Scene {
 	public:
+		Entity createEntity();
+		void destroyEntity(const Entity& entity);
+
+	private:
+		entt::registry m_entityRegistry;
+
+		friend class Entity;
+		friend class Application;
 	};
 
 }
