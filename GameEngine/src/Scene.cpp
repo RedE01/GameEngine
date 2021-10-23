2a8ecb0e029d302e839a36568c76be570cbfd456
RedE01 <e.sternvik@gmail.com>
2021-10-23 15:30:42 +0200

    Delete Component, GameObject and ResourceArray

--- GameEngine/src/Scene.cpp
+++ GameEngine/src/Scene.cpp
@@ -2,29 +2,4 @@
 
 namespace GameEngine {
 
-	Scene::~Scene() {
-		m_gameObjects.clear();
-	}
-
-	GameObject* Scene::createGameObject() {
-		GameObject* go = m_gameObjects.create();
-		go->init();
-		return go;
-	}
-
-	bool Scene::removeGameObject(Resource<GameObject> gameObjectResource) {
-		return m_gameObjects.remove(gameObjectResource.ID);
-	}
-
-	std::optional<GameObject*> Scene::getGameObject(Resource<GameObject> gameObjectResource) {
-		return m_gameObjects.get(gameObjectResource.ID);
-	}
-
-	void Scene::updateGameObjects() {
-		for(auto& go : m_gameObjects) {
-			go.update();
-		}
-	}
-
-
 }
--- GameEngine/src/Scene.h
+++ GameEngine/src/Scene.h
@@ -1,22 +1,9 @@
 #pragma once
-#include "GameObject.h"
-#include "Util/ResourceArray.h"
-#include "Util/Resource.h"
 
 namespace GameEngine {
 
 	class Scene {
 	public:
-		~Scene();
-
-		GameObject* createGameObject();
-		bool removeGameObject(Resource<GameObject> gameObjectResource);
-		std::optional<GameObject*> getGameObject(Resource<GameObject> gameObjectResource);
-
-		void updateGameObjects();
-
-	private:
-		ResourceArray<GameObject> m_gameObjects;
 	};
 
 }
